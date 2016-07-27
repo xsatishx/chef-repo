6ab71485874380fac953b86fedf96cddd9c7b9bc
Satish <xsatishx@gmail.com>
2016-07-27 12:01:57 +0800

    Initial repo

---
+++ cookbooks/mysql-server-5.5/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: mysql-server-5.5
+# Recipe:: default
+#
+# Copyright 2016, HealthSeq Asia Pvt. Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+
+include_recipe 'mysql-server-5.5::pdcv3'
---
+++ cookbooks/nova-keystone-pdc/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: nova-keystone-pdc
+# Recipe:: default
+#
+#  Copyright 2016, HealthSeq Asia Pvt. Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'nova-keystone-pdc:pdcv3'
