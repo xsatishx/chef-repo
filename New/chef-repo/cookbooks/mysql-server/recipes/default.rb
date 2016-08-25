3c6f8837ba79823f2aa1ecdbb1511e4abc941d46
Satish <xsatishx@gmail.com>
2016-08-25 15:39:22 +0800

    some corrections

--- New/chef-repo/cookbooks/mysql-server/recipes/default.rb
+++ New/chef-repo/cookbooks/mysql-server/recipes/default.rb
@@ -10,31 +10,55 @@
 # TO DO: Modify files/mysql-seed with the password
 # TO DO: Modify templates/mysqld_openstack.cnf.erb with the bind ip
 
+bash "update-apt-repository" do
+  user "root"
+  code <<-EOH
+  apt-get update
+  EOH
+end
+
+cookbook_file '/tmp/mysql-seed' do
+  source 'mysql-seed'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+# No template or cookbook file found for response file  = just use bash as below
 package "mysql-server-5.5" do
   action :install
-  response_file "mysql-seed.erb"
+  response_file '/tmp/mysql-seed'
   notifies :create, "template[/etc/mysql/conf.d/mysqld_openstack.cnf]", :immediately
 end
 
+
+# bash 'install-mysql-server' do
+#   user 'root'
+#   code <<-EOH
+#     sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password healthseq'
+#     sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password healthseq'
+#     sudo apt-get -y install mysql-server-5.5
+#   EOH
+# end
+
+
+template '/etc/mysql/conf.d/mysqld_openstack.cnf' do
+  source 'mysqld_openstack.cnf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+# test run could not find this package on aws
 package 'python-pymysql' do
   action :install
 end
 
-template "/etc/mysql/conf.d/mysqld_openstack.cnf" do
-  mode "440"
-  owner "root"
-  group "root"
-  source "mysql_overrides.cnf.#{node.chef_environment}.erb"
-  variables(
-  )
-  action :create
-  notifies :restart , "service[mysql]", :immediately
-end
 
 service "mysql" do
   provider Chef::Provider::Service::Init::Debian
   supports :status => true, :restart => true, :stop => true, :start => true
-  action [:enable, :start]
+  action [:enable]
 end
 
 bash 'secure installation' do
@@ -42,4 +66,4 @@ bash 'secure installation' do
   code <<-EOH
   mysql_secure_installation
   EOH
-endmys
+end
