43ce59caae195917080131a8eabaa9836df6093e
xsatishx <xsatishx@gmail.com>
2020-02-17 16:17:46 +0800

    Update default.rb

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -1,33 +1,23 @@
-#
 # Cookbook Name:: wordpress
 # Recipe:: default
-#
-# # Maintainer Satish Balakrishnan <satish@healthseq.com>
-#
-# All rights reserved - Do Not Redistribute
-#
+# Maintainer Satish Balakrishnan
 
-
-# Install required packages
 for packages in [ "apache2","curl","php-curl", "php-gd", "php-mbstring", "php-mcrypt", "php-xml", "php-xmlrpc", "mysql-client"] do
   package packages do
     action :install
   end
 end
 
-#download wordpress
 remote_file "/tmp/latest.tar.gz" do
    source "https://wordpress.org/latest.tar.gz"
 end
 
-# untar
 execute 'untar-wordpress' do
   command 'tar xzvf latest.tar.gz'
   cwd '/tmp'
   not_if { File.exists?("/tmp/wordpress/wp-config-sample.php") }
 end
 
-#copy wordpress dir to /var/www
 bash 'copy-wordpress-www' do
   user 'root'
   cwd '/tmp'
